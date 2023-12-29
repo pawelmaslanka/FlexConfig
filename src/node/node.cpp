3cc7da2f84d7fb215598e3922c3bf8cfc3488d2b
Pawel Maslanka <pmaslank@akamai.com>
2023-12-29 18:49:15 +0100

    Updated all files about license header

--- src/node/node.cpp
+++ src/node/node.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2024 Pawel Maslanka (pawmas@hotmail.com)
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, version 3.
+ */
 #include "node.hpp"
 #include "leaf.hpp"
 
